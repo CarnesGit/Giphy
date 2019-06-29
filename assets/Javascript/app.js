4bfa2e298cb0c1624fb45b1022035d5d8f87e918
Rick <rick.carnes@gmail.com>
2019-06-28 20:20:08 -0400

    more debugging

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -9,20 +9,20 @@ $(document).ready(function() {
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        // currentResponse = response
-        // console.log("Current Response", currentResponse)
-        // console.log("response", response);
-        // console.log(response.data[0].images.original_still.url);
-        // $('#gif1').attr("src", currentResponse.data[0].images.original_still.url);
-        // $('#gif2').attr("src", currentResponse.data[1].images.original_still.url);
-        // $('#gif3').attr("src", currentResponse.data[2].images.original_still.url);
-        // $('#gif4').attr("src", currentResponse.data[3].images.original_still.url);
-        // $('#gif5').attr("src", currentResponse.data[4].images.original_still.url);
-        // $('#gif6').attr("src", currentResponse.data[5].images.original_still.url);
-        // $('#gif7').attr("src", currentResponse.data[6].images.original_still.url);
-        // $('#gif8').attr("src", currentResponse.data[7].images.original_still.url);
-        // $('#gif9').attr("src", currentResponse.data[8].images.original_still.url);
-        // $('#gif10').attr("src", currentResponse.data[9].images.original_still.url);
+        currentResponse = response
+        console.log("Current Response", currentResponse)
+        console.log("response", response);
+        console.log(response.data[0].images.original_still.url);
+        $('#gif1').attr("src", currentResponse.data[0].images.original_still.url);
+        $('#gif2').attr("src", currentResponse.data[1].images.original_still.url);
+        $('#gif3').attr("src", currentResponse.data[2].images.original_still.url);
+        $('#gif4').attr("src", currentResponse.data[3].images.original_still.url);
+        $('#gif5').attr("src", currentResponse.data[4].images.original_still.url);
+        $('#gif6').attr("src", currentResponse.data[5].images.original_still.url);
+        $('#gif7').attr("src", currentResponse.data[6].images.original_still.url);
+        $('#gif8').attr("src", currentResponse.data[7].images.original_still.url);
+        $('#gif9').attr("src", currentResponse.data[8].images.original_still.url);
+        $('#gif10').attr("src", currentResponse.data[9].images.original_still.url);
 
 
         $('#scienceFiction').click(function() {
